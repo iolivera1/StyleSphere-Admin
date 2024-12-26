966e9d2363109e4e6f8e76f3986b277648d41df3
Iván <ivan.olivera@boronstudio.com>
2024-12-26 01:41:43 -0300

    fixes on middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,21 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId, redirectToSignIn } = await auth()
+    const { userId, redirectToSignIn } = await auth();
 
-  if (!userId) {
-    return redirectToSignIn()
-  }
-})
+    const requestPath = req.nextUrl.pathname;
+    const isPublicRoute = requestPath.includes('/api/');
+
+    if (!userId && !isPublicRoute) {
+        return redirectToSignIn();
+    }
+
+    return;
+});
 
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
-}
+    matcher: [
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        '/(api|trpc)(.*)',
+    ],
+};
