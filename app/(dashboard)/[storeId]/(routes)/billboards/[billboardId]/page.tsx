348141fce54e36a594ec651e1d05b5d285a3239c
Iván <ivan.olivera@boronstudio.com>
2024-12-26 22:50:51 -0300

    fixes

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,24 +1,25 @@
 import prismadb from "@/lib/prismadb";
 
-import {BillboardForm} from "./components/billboard-form";
+import { BillboardForm } from "./components/billboard-form";
 
+// Explicitly defining the type for `params` as an object containing `billboardId`
 interface BillboardPageProps {
-    params: { billboardId: string };
+    params: {
+        billboardId: string;
+    };
 }
 
-export default async function BillboardPage({params}: BillboardPageProps) {
-    const {billboardId} = await params;
-
+export default async function BillboardPage({ params }: BillboardPageProps) {
     const billboard = await prismadb.billboard.findUnique({
         where: {
-            id: billboardId,
+            id: params.billboardId,
         },
     });
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-6 pt-6">
-                <BillboardForm initialData={billboard}/>
+                <BillboardForm initialData={billboard} />
             </div>
         </div>
     );
