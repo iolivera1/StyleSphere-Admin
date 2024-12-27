9811004b369c714bc49ab33f588ae4967e5e621f
Iván <ivan.olivera@boronstudio.com>
2024-12-26 22:35:56 -0300

    fixes on billboard

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -3,22 +3,25 @@ import prismadb from "@/lib/prismadb";
 import { BillboardForm } from "./components/billboard-form";
 
 export default async function BillboardPage({
-  params,
-}: {
+                                              params,
+                                            }: {
   params: { billboardId: string };
 }) {
-  const { billboardId } = await params;
+  // Remove the unnecessary await on params
+  const { billboardId } = params;
 
+  // Fetch the billboard data from the database
   const billboard = await prismadb.billboard.findUnique({
     where: {
-      id: billboardId
+      id: billboardId,
     },
   });
+
   return (
-    <div className="flex-col">
-      <div className="flex-1 space-y-4 p-6 pt-6">
-        <BillboardForm initialData={billboard} />
+      <div className="flex-col">
+        <div className="flex-1 space-y-4 p-6 pt-6">
+          <BillboardForm initialData={billboard} />
+        </div>
       </div>
-    </div>
   );
 }
